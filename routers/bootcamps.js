46f422b9b20a42977c201112872080f1f400903e
saurav2811 <saurav.sri2811@gmail.com>
2021-05-02 03:13:58 +0530

    advance result added but average cost is not working

--- routers/bootcamps.js
+++ routers/bootcamps.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const router = express.Router();
-courseRouter=require('./courses')
 const {getBootcamps,getBootcamp,createBootcamps,updateBootcamps,deleteBootcamps,GetWithInRadius,bootcampPhotoUpload}=require('../controller/bootcamps')
+const Bootcamp= require ('../models/Bootcamp')
+const advancedResults=require('../middleware/advancedResults')
+
+courseRouter=require('./courses')
+const router = express.Router();
 //used course router
 router.use('/:bootcampId/courses',courseRouter)
 //one way of getting thing from controller nothing way writting code in one line
 router.route('/radius/:zipcode/:distance').get(GetWithInRadius)
-router.route('/').get(getBootcamps).post(createBootcamps)
+router.route('/').get(advancedResults(Bootcamp,'courses'),getBootcamps).post(createBootcamps)
 //way of writing in an otherway
 router.route('/:id')
 .get(getBootcamp)
