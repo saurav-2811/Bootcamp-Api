b37c37376f65e823e252d97099cc14b033615ecb
saurav2811 <saurav.sri2811@gmail.com>
2021-05-04 02:00:45 +0530

    some change in middleware sendTokenResponse

--- controller/auth.js
+++ controller/auth.js
@@ -45,8 +45,12 @@ const sendTokenResponse=(user,statusCode,res)=>{
     const token=user.getJwtToken()
     const options={
         expires:new Date(Date.now()+process.env.cookieexpire*24*60*60*1000),
-        httpOnly:false
+        httpOnly:true
     };
+   
+    if(process.env.NODE_ENV==='production'){
+        options.secure=true
+    }
     res
     .status(statusCode)
     .cookie('token',token,options)
